214d325091194105733d9605b02ac63ce2f46e5d
Dean Gurvitz <dean.gurvitz@planckresolution.com>
2023-03-05 18:15:17 +0200

    Test refactoring and additions

--- src/test/java/com/github/deansg/jeocodio/GeocodioClientTests.java
+++ src/test/java/com/github/deansg/jeocodio/GeocodioClientTests.java
@@ -1,21 +1,26 @@
 package com.github.deansg.jeocodio;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
+import java.io.IOException;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GeocodioClientTests {
+    private static final String EXPECTED_DEFAULT_BASE_URL_SCHEME = "https";
+    private static final String EXPECTED_DEFAULT_BASE_URL_HOST = "api.geocod.io";
     private String randomApiKey;
     private HttpClient httpClient;
     private GeocodioClient geocodioClient;
@@ -28,21 +33,30 @@ public class GeocodioClientTests {
     }
 
     @Test
-    public void foo() throws ExecutionException, InterruptedException {
-        var argumentCaptor = ArgumentCaptor.forClass(HttpRequest.class);
-        var mockFuture = CompletableFuture.completedFuture(mockHttpResponse("{}"));
+    public void testGeocodeAsyncSanity() throws ExecutionException, InterruptedException, IOException {
+        var rawResponse = readSampleGeocodingResponse();
+        var inputQ = "1109 N Highland St. Arlington VA";
+        var mockFuture = CompletableFuture.completedFuture(mockHttpResponse(rawResponse));
         when(httpClient.sendAsync(any(), eq(HttpResponse.BodyHandlers.ofString()))).thenReturn(mockFuture);
 
-        var future = geocodioClient.geocodeAsync("");
+        var future = geocodioClient.geocodeAsync(inputQ);
 
-        Assertions.assertNotNull(future);
+        assertNotNull(future);
         var result = future.get();
-        Assertions.assertNotNull(result);
+        assertNotNull(result);
+        validateGeocodeRequest(inputQ);
+    }
+
+    private void validateGeocodeRequest(String inputQ) {
+        var argumentCaptor = ArgumentCaptor.forClass(HttpRequest.class);
         verify(httpClient).sendAsync(argumentCaptor.capture(), any());
         var actualRequest = argumentCaptor.getValue();
+        assertEquals("GET", actualRequest.method());
         var actualUri = actualRequest.uri();
-        var actualQuery = actualUri.getQuery();
-        Assertions.assertTrue(actualQuery.contains("api_key=" + randomApiKey));
+        assertEquals(EXPECTED_DEFAULT_BASE_URL_SCHEME, actualUri.getScheme());
+        assertEquals(EXPECTED_DEFAULT_BASE_URL_HOST, actualUri.getHost());
+        assertEquals("/v1.7/geocode", actualUri.getPath());
+        assertEquals(String.format("api_key=%s&q=%s", randomApiKey, encodeUrlComponent(inputQ)), actualUri.getQuery());
     }
 
     private <T> HttpResponse<T> mockHttpResponse(T content) {
@@ -50,5 +64,13 @@ public class GeocodioClientTests {
         HttpResponse<T> mockResponse = mock(HttpResponse.class);
         when(mockResponse.body()).thenReturn(content);
         return mockResponse;
+    }
+
+    private String readSampleGeocodingResponse() throws IOException {
+        return TestUtils.readResource("sample_geocoding_response.json");
+    }
+
+    private String encodeUrlComponent(String component) {
+        return URLEncoder.encode(component, StandardCharsets.UTF_8);
     }
 }
