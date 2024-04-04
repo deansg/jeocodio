1a29d3f7cacd41b7031a63369d8e3aaba8a6650f
Chuck May <mayc@imsweb.com>
2024-04-04 13:32:25 -0400

    Support the "_warnings" field

--- src/main/java/io/github/deansg/jeocodio/models/GeocodingResponse.java
+++ src/main/java/io/github/deansg/jeocodio/models/GeocodingResponse.java
@@ -2,6 +2,9 @@ package io.github.deansg.jeocodio.models;
 
 import java.util.List;
 
+import com.google.gson.annotations.SerializedName;
+
 public record GeocodingResponse(GeocodingResponseInput input,
-                                List<GeocodingResult> results) {
+                                List<GeocodingResult> results,
+                                @SerializedName("_warnings") List<String> warnings) {
 }
--- src/test/java/io/github/deansg/jeocodio/IntegrationTest.java
+++ src/test/java/io/github/deansg/jeocodio/IntegrationTest.java
@@ -1,13 +1,17 @@
 package io.github.deansg.jeocodio;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.ExecutionException;
+import io.github.deansg.jeocodio.models.GeocodingRequestBuilder;
 
 public class IntegrationTest {
+
     private static final String API_KEY = System.getenv("API_KEY");
     private GeocodioClient geocodioClient;
 
@@ -19,7 +23,7 @@ public class IntegrationTest {
     @Disabled("Should be used for local testing with a real API key as an environment variable")
     @Test
     public void testGeocodingE2E() throws ExecutionException, InterruptedException {
-         var q = "1109 N Highland St, Arlington VA";
+        var q = "1109 N Highland St, Arlington VA";
 
         var future = geocodioClient.geocodeAsync(q);
 
@@ -27,4 +31,20 @@ public class IntegrationTest {
         Assertions.assertNotNull(result);
         Assertions.assertEquals(1, result.results().size());
     }
+
+    @Disabled("Should be used for local testing with a real API key as an environment variable")
+    @Test
+    public void testGeocodingE2EWarnings() throws ExecutionException, InterruptedException {
+        // test warnings
+        var geocodingRequest = GeocodingRequestBuilder.builder()
+                .q("Road X, Hot Springs, AR")
+                .fields(List.of("acs-social"))
+                .limit(1)
+                .build();
+        var result = geocodioClient.geocodeAsync(geocodingRequest).get();
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(1, result.results().size());
+        Assertions.assertEquals(List.of("acs-social was skipped since result is not street-level"), result.results().get(0).warnings());
+    }
+
 }
